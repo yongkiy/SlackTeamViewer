4a7bee86422959f02cef28e23c227fba7e614688
yongkiy <yusmanthia.y@gmail.com>
2016-08-06 13:23:09 -0700

    Update STVConstants.h

--- SlackTeamViewer/STVConstants.h
+++ SlackTeamViewer/STVConstants.h
@@ -2,9 +2,6 @@
 //  STVConstants.h
 //  SlackTeamViewer
 //
-//  Created by Yongki Yusmanthia on 7/13/15.
-//  Copyright (c) 2015 Yongki Yusmanthia. All rights reserved.
-//
 
 #define kBackgroundColor [UIColor colorWithRed:0.98 green:0.98 blue:1.0 alpha:1.0]
 #define kTextColor [UIColor blueColor]
