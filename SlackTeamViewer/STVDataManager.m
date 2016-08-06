c681e80a9dd09b789a8ed5603f297d84104a73f9
yongkiy <yusmanthia.y@gmail.com>
2016-08-06 13:23:49 -0700

    Update STVDataManager.m

--- SlackTeamViewer/STVDataManager.m
+++ SlackTeamViewer/STVDataManager.m
@@ -2,9 +2,6 @@
 //  STVDataManager.m
 //  SlackTeamViewer
 //
-//  Created by Yongki Yusmanthia on 7/12/15.
-//  Copyright (c) 2015 Yongki Yusmanthia. All rights reserved.
-//
 
 #import "STVDataManager.h"
 
